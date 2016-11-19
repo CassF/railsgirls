0ac5d85ee2b6d9b3e81f5b89506bc595b22b2d01
Tech-A40 <Tech-A40@Tech-A40s-MacBook-Air.local>
2016-11-19 15:44:34 +0000

    created a new page called about

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/info'
+  get 'pages/about'
 
 	root to: redirect('/ideas')
   resources :ideas
+  resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
